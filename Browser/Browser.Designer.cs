462d019192be9aa6fbe564fc2415695dda11035c
ufukgomecoglu <ufuk.gomecoglu@gmail.com>
2022-09-15 11:30:05 +0300

    .

--- Browser/Browser.Designer.cs
+++ Browser/Browser.Designer.cs
@@ -104,7 +104,6 @@
             this.TSTB_Adres.Margin = new System.Windows.Forms.Padding(5, 0, 1, 0);
             this.TSTB_Adres.Name = "TSTB_Adres";
             this.TSTB_Adres.Size = new System.Drawing.Size(550, 25);
-            this.TSTB_Adres.Text = "face";
             this.TSTB_Adres.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TSTB_Adres_KeyDown);
             // 
             // toolStripDropDownButton1
